0d8f52054112ba133d1d3336d635011f4a24fe38
Hugo Gonzalez <hugo.gonzales@mirpartners.com>
2015-11-24 14:34:10 -0800

    initial commit

---
+++ fishes/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework import permissions
+from fishes.models import Fish
+from fishes.serializers import FishSerializer
+
+# Create your views here.
+class FishViewSet(viewsets.ModelViewSet):
+    # this fetches all the rows of data in the Fish table
+    queryset = Fish.objects.all()
+    serializer_class = FishSerializer
